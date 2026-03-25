5b73f4ec024c158edf5978dce0039894a42a681c
carlossepulvedadiaz <cssepulvedadiaz@gmail.com>
2026-03-25 13:31:52 -0300

    nuevo.2

--- bibliotecaduoc/bibliotecaduoc/src/main/java/com/example/bibliotecaduoc/repository/LibroRepository.java
+++ bibliotecaduoc/bibliotecaduoc/src/main/java/com/example/bibliotecaduoc/repository/LibroRepository.java
@@ -12,9 +12,61 @@ public class LibroRepository {
 
     //guarda todos los libros 
     private List<Libro> listaLibros = new ArrayList<>();
-    
+
     //metodo que retorna todos los libros
     public List<Libro> obteneLibros() {
         return listaLibros;
+
     }
+
+    //Buscar libro por su id
+    public Libro buscarPorId(int id){
+        for(Libro libro :listaLibros ){
+            if (libro.getId() ==id){
+                return libro;
+            }
+        }
+        return null;
+    }
+
+    public Libro buscarPorIsbn(String isbn) {
+        for (Libro libro : listaLibros) {
+            if (libro.getIsbn().equals(isbn)){
+                return libro;
+            }
+        }
+        return null;
+    }
+
+    public Libro guardar(Libro lid) {
+        listaLibros.add(lib);
+        return lib;
+    }
+
+    public Libro actualizar(Libro lib){
+        int id = 0;
+        int idPosicion = 0;
+
+        for (int i = 0; i <listaLibros.size(); i++){
+            if (listaLibros.get(i).getId() == lib.getId()){
+                id = lib.getId();
+                idPosicion = i;
+            }
+        }
+    }
+    Libro.libro1 = new Libro();
+    libro1.setId(id):
+    libro1.setTitulo(lib.getTitulo());
+    libro1.setAutor(lib.getAutor());
+    libro1.setFechaPublicacion(lib.getFechaPublicacion());
+    libro1.setEditorial(lib.getEditorial());
+    libro.setIsbn(lib.getUsbn());
+
+    listaLibros.set(idPosicion, libro1);
+    return libro1;
+
+
+
+
 }
+
